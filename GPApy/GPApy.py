c45663f5658845500fdd9182c7257758db328505
Matthew Boatright <matthew.c.boatright@gmail.com>
2015-03-06 21:13:30 -0500

    Detects left recursion.
    Will pick up any instances of left recursion and then report it.

--- GPApy/GPApy.py
+++ GPApy/GPApy.py
@@ -8,6 +8,8 @@
 import sys
 
 grammar_dictionary = {}
+left_recursion = []
+checked = []
 
 def main():
     
@@ -27,27 +29,42 @@ def read(input):
     file = open(input, 'r')
     lines = file.readlines()
     
+    # Read the file line by line has insert the values into their proper place.
     print("Selected Grammar")
     print("----------------")
-
     for i in range (0, len(lines)):
         if (lines[i].strip() != ""):
             lines[i] = lines[i].strip().replace("@", "empty")
             print(lines[i])
-            tokens = lines[i].replace("->", "").replace("|", "").split()
-            for j in range (0, len(tokens)):
-                tk = tokens[j].strip()
-                if (j == 0):
-                    head = tk
-                    #print("Nonterminal: ", tk)
-                    input_nt(head)
-                else:
-                    #print("Leads to rule: ", tk)
-                    input_rule(head, tk)
+            tokens = lines[i].split("->")
+            head = tokens.pop(0).strip()
+            tokens = tokens.pop(0).strip().split("|")
+            input_nt(head)
+            input_rule(head, tokens)
 
+    # Print out the collected grammar rules from the dictionary and display them.
+    # Also send them through the left_rec function to determine if left recursion exists.
     print("\n")
+    print("Grammar Dictionary")
+    print("------------------")
     for key in sorted(grammar_dictionary):
         print(key, grammar_dictionary[key])
+        checked = []
+        checked.append(key)
+        if (left_rec(key, grammar_dictionary[key], checked)):
+            left_recursion.append(key)
+    print("\n")
+
+    # Now that the grammar has been completely read and inserted into the dictionary we can begin examining the grammar.
+    print("Left Recursion")
+    print("--------------")
+    if left_recursion:
+        for key in left_recursion:
+            print(key, "has left recursion")
+    else:
+        print("No instances of left recursion found in the grammar.")
+    print("\n")
+
 
 # In this method, a non-terminal is passed. It is then added to the global grammar_dictionary variable.
 # If it already exists, the method will do nothing. Otherwise it will be added and given a default value by 0.
@@ -63,12 +80,38 @@ def input_nt(nt):
 def input_rule(head, rule):
     #print("Adding ", rule, " to ", head)
     if (grammar_dictionary[head] == 0):
-        lst = [rule]
-        grammar_dictionary[head] = lst
+        grammar_dictionary[head] = rule
     else:
-        lst = grammar_dictionary[head]
-        if (lst.count(rule) != 1):
-            lst.append(rule)
+        while (len(rule) > 0):
+            lst = grammar_dictionary[head]
+            tmp = rule.pop().strip()
+            if (lst.count(tmp) != 1):
+                lst.append(tmp)
+
+# A function to determine the presence of left-recursion (including non-immediate) in the grammar.
+# The parameters are as follows: 
+#   key - the non-terminal we are looking for. If at any point the first value of a rule matches up with the key then left recursion exists.
+#   lst - the current list that is being examined. This is a recursive function and the same key may be compared to multiple lists.
+def left_rec(key, lst, chk):
+    #print("Browsing", lst, "for", key)
+    for rule in lst:
+        if rule:
+            val = rule.split().pop(0)
+            #print("---Looking at", val)
+            if (val == key):
+                #print("Left recursion exists for key", key)
+                return True
+            if val in grammar_dictionary:
+                #print("---", val, "is a non-terminal. Expanding search into it's rules.")
+                if val not in chk:
+                    chk.append(val)
+                    if (left_rec(key, grammar_dictionary[val], chk)):
+                        return True
+    #print("No instances of left recusrion found for key", key)
+    return False
+
+def factor():
+    return False
 
 if __name__ == "__main__":
     main()
